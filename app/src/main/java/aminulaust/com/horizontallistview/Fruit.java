49672c16e56b339b847a3b005ba3fc91dde7dab5
Aminul <aminulaust@gmail.com>
2017-05-14 22:14:10 +0600

    first commit

---
+++ app/src/main/java/aminulaust/com/horizontallistview/Fruit.java
@@ -0,0 +1,42 @@
+package aminulaust.com.horizontallistview;
+
+
+public class Fruit {
+
+    String cardName;
+    int imageResourceId;
+    int isfav;
+    int isturned;
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+
+    public int getIsfav() {
+        return isfav;
+    }
+
+    public void setIsfav(int isfav) {
+        this.isfav = isfav;
+    }
+
+    public int getIsturned() {
+        return isturned;
+    }
+
+    public void setIsturned(int isturned) {
+        this.isturned = isturned;
+    }
+}
---
+++ app/src/main/java/aminulaust/com/horizontallistview/MainActivity.java
@@ -0,0 +1,25 @@
+package aminulaust.com.horizontallistview;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new HorizontalListViewFragment();
+            ;
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
